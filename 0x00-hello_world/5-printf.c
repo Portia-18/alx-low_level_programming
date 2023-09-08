51641ea977608067e0519e7c29278e2d1bca5949
Portia-18 <portiagatihi@gmail.com>
2023-09-08 13:11:07 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,10 +7,10 @@
  *
  * n adds a new line
  *
- *Return; always 0
-*/ 
+ *Return: always 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
