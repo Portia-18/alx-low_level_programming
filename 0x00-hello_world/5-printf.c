1c44cae39928c0bc3c507ed43e8adce4c971748a
Portia-18 <portiagatihi@gmail.com>
2023-09-08 13:00:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - it executes the program
+ *
+ * printf - prints string
+ *
+ * n adds a new line
+ *
+ *Return; always 0
+*/ 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
