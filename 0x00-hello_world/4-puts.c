69f3a8d34c6266988e6d49848187b97c12247ab9
Portia-18 <portiagatihi@gmail.com>
2023-09-07 18:20:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main (void)
+/**
+*main - this is the main function
+*
+*Return: always 0
+*/
+
+int main(void)
 {
-	char message[] = "Programming is like building a multilingual puzzle";
-	puts(message);
-	return 0;
-}
-
+puts("\"Programming is like building a multilingual puzzle)
+return (0);
+}  
