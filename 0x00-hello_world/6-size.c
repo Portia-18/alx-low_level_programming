de2fa1ea1288d7b30e6bfa4228d033c7bda039cb
Portia-18 <portiagatihi@gmail.com>
2023-09-08 14:18:59 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main - it executes the program
+ *
+ * printf - displays the string
+ *
+ * n - adds a new line
+ *
+ * sizeof - gets storage size of various types in bytes
+ *
+ * Return: always 0
+ *
+*/
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an double: %lu byte(s)\n", sizeof(double));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
