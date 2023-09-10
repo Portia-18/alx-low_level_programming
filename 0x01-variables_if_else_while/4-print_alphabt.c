611ba647934a5915763b34c6caa91721bac75f34
Portia-18 <portiagatihi@gmail.com>
2023-09-10 16:33:51 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - executes prog
+ *
+ * putchar - writes to std output
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch > 96 && ch < 123; ch++)
+	{
+		if (ch != 101 && ch != 113) 
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
