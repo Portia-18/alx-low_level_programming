df5bdac4bbc1d284e2bfea72a1ebcaced20c7aa5
Portia-18 <portiagatihi@gmail.com>
2023-09-10 16:50:37 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - executes program
+ *
+ * putchar - writes to stdo
+ *
+ * for - repeats code
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
