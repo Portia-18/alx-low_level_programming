7a703ed686a6ca52effccaf7159b270ed32f82ec
Portia-18 <portiagatihi@gmail.com>
2023-09-11 05:23:43 -0700

    base 16 no.s in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * putchar - writes stdo
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int a;
+
+	for (i = 48; i < 58; i++)
+	putchar(i);
+	for (a = 97; a < 103; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
+
