7aba52cb40986c483047e417c757c5022fc6e9e8
Portia-18 <portiagatihi@gmail.com>
2023-09-12 14:33:40 -0700

    prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - gives no.s signs
+ *
+ * @n: entry point
+ *
+ * Return: 1 if true and 0
+ */
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+	return (1);
+	else if(n == 48)
+	{
+	return (0);
+	else
+	{
+	return (-1);
+	}
+	}
+	}
+}
