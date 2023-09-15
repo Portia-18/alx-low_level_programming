279827b850b531f2a226b1884d9d5ea99c73eb27
Portia-18 <portiagatihi@gmail.com>
2023-09-15 15:03:59 -0700

    print digit as 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _isupper - prints only in uppercase
  *
@@ -13,7 +12,7 @@ int _isupper(int c)
 {
 
 	if (c >= 65 && c <= 90)
-	return(1);
+	return (1);
 	else
 	return (0);
 }
