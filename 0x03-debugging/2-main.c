0d5d7e5a1e0692d13f3643550c1ceef9b267eba4
Portia-18 <portiagatihi@gmail.com>
2023-09-15 14:44:44 -0700

    largest no.

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -6,6 +6,24 @@
  * Return: 0
  */
 
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && b >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
 int main(void)
 {
 	int a, b, c;
