9e780baacae8b2e54b6273bff8697a5d61145ca7
Portia-18 <portiagatihi@gmail.com>
2023-09-17 11:14:12 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - prints _
+ * @n: input
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{/* */
+		_putchar('\n');
+	}
+	else/* */
+	{
+		for (; n > 0; n--)
+		{
+			_putchar('_');
+			_putchar('\n');
+		}
+	}
+}
