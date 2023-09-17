6741e0172ca6bb2b9cf1824b0039a66b0b114fab
Portia-18 <portiagatihi@gmail.com>
2023-09-17 11:48:03 -0700

    diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints \
+ * @n: input
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (; n >= 1, n--)
+			{
+				_putchar(' ');
+				_putchar('\');
+		}
+	_putchar('\n');
+	}
+}
