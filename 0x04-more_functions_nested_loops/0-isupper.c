19b9d4676040be46d01d20c53f3029d889f44d6e
Portia-18 <portiagatihi@gmail.com>
2023-09-17 10:26:49 -0700

    check for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,14 +5,16 @@
  * @c:
  *
  * Return: 1 is true 0 false
- *
- * Return: 0 is success
  */
 int _isupper(int c)
 {
 
 	if (c >= 65 && c <= 90)
-	return (1);
+	{
+		return (1);
+	}
 	else
-	return (0);
+	{
+		return (0);
+	}
 }
