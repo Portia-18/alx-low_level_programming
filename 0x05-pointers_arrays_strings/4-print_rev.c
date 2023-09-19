f5ee0683798798ad4f0482e53dfdac2493b1ee06
Portia-18 <portiagatihi@gmail.com>
2023-09-19 13:54:01 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - reverses
+ * @s : string
+ */
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; ++index);
+
+	for (--index; index >= 0; --index)
+		_putchar(s[index]);
+}
