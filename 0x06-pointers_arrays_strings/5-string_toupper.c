101cbe087c3072fdea1a3ed782a750764329ad5e
Portia-18 <portiagatihi@gmail.com>
2023-09-24 13:47:03 -0700

    late night talking

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - converts strings to uppercase
+ * @c: string parameter
+ * Return: the converted uppercase array
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+		i++;
+	}
+	return (c);
+}
